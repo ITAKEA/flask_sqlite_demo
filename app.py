39cf804a28a7c988de19bf8b1e38d7f4fc7c0451
clbokea <clbo@kea.dk>
2025-09-19 10:19:51 +0200

    faker added

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
+from data_dict_simple import students
+from data_dict import random_users
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return jsonify({'name':'Claus'})
+
+@app.route('/api/students', methods=['GET'])
+def read():
+    return jsonify(random_users)
+
+@app.route('/api/students', methods=['POST'])
+def create():
+    random_users.append({'id':3, 'name': 'Pia'})
+    return jsonify(None)
 
 app.run()
---
+++ data_dict_simple.py
@@ -0,0 +1,2 @@
+
+students = [ {'id':1, 'name': 'Claus'}, {'id':2, 'name':'Ida'} ]
